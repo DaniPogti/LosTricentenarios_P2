7e6e28066db7f57a5c0581df28f075b24f19652b
Daniel Portillo <d.portillo.4adb@gmail.com>
2025-03-21 23:23:09 -0600

    Commit

--- Programa/main.py
+++ Programa/main.py
@@ -27,7 +27,6 @@ mp_drawing_styles = mp.solutions.drawing_styles #estilos para dibujar los puntos
 mp_hands = mp.solutions.hands #modelo de manos
 
 cap = cv2.VideoCapture(0) #captura de video
-#cap = cv2.VideoCapture('c:/Users/Portillo/Desktop/USAC 2023/Samsumg/Proyecto 2/LosTricentenarios_P2/DataSet/A/A1.jpg')
 cap.set(3,1920) #establece el ancho de la imagen
 cap.set(4,1080) #establece el largo de la imagen
 with mp_hands.Hands( #modelo de manos
@@ -101,19 +100,15 @@ with mp_hands.Hands( #modelo de manos
                     middle_finger_tip[1] > middle_finger_pip[1] and \
                     ring_finger_tip[1] > ring_finger_pip[1] and \
                     pinky_tip[1] > pinky_pip[1]:
-                        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa")
-                        cv2.putText(image, 'A', (700, 150), 
-                                    cv2.FONT_HERSHEY_SIMPLEX, 
-                                    3.0, (0, 0, 255), 6)
+                        print("letra detectada ===== A")
+
                     
                 #letra B  
                 elif index_finger_pip[1] - index_finger_tip[1]>0 and pinky_pip[1] - pinky_tip[1] > 0 and \
                     middle_finger_pip[1] - middle_finger_tip[1] >0 and ring_finger_pip[1] - ring_finger_tip[1] >0 and \
                         middle_finger_tip[1] - ring_finger_tip[1] <0 and abs(thumb_tip[1] - ring_finger_pip2[1])<40:
-                    print("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
-                    cv2.putText(image, 'B', (700, 150), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 
-                                3.0, (0, 0, 255), 6)
+                    print("letra detectada ===== B")
+
                 #letra C    
                 
                 elif abs(index_finger_tip[1] - thumb_tip[1]) < 50 and \
@@ -124,10 +119,7 @@ with mp_hands.Hands( #modelo de manos
                     middle_finger_tip[0] < middle_finger_pip[0] and \
                     ring_finger_tip[0] < ring_finger_pip[0] and \
                     pinky_tip[0] < pinky_pip[0]:
-                    print("cccccccccccccccccc")
-                    cv2.putText(image, 'C', (700, 150), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 
-                                3.0, (0, 0, 255), 6)    
+                    print("letra detectada ===== C")   
                     
                 
                 #letra D
@@ -135,10 +127,8 @@ with mp_hands.Hands( #modelo de manos
                     and distancia_euclidiana(thumb_tip, ring_finger_tip) < 65 \
                     and  pinky_pip[1] - pinky_tip[1]<0\
                     and index_finger_pip[1] - index_finger_tip[1]>0:
-                    print("DDDDDDDDDDDD")
-                    cv2.putText(image, 'D', (700, 150), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 
-                                3.0, (0, 0, 255), 6) 
+                    print("letra detectada ===== D")
+
                 #letra E   
                 elif index_finger_pip[1] - index_finger_tip[1] < 0 and pinky_pip[1] - pinky_tip[1] < 0 and \
                     middle_finger_pip[1] - middle_finger_tip[1] < 0 and ring_finger_pip[1] - ring_finger_tip[1] < 0 \
@@ -147,18 +137,14 @@ with mp_hands.Hands( #modelo de manos
                             and thumb_tip[1] - middle_finger_tip[1] > 0 \
                             and thumb_tip[1] - ring_finger_tip[1] > 0 \
                             and thumb_tip[1] - pinky_tip[1] > 0:
-                    print("EEEEEEEEEEEEEEEEE")
-                    cv2.putText(image, 'E', (700, 150), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 
-                                3.0, (0, 0, 255), 6)
+                    print("letra detectada ===== E")
+
                 #letra F   
                 elif  pinky_pip[1] - pinky_tip[1] > 0 and middle_finger_pip[1] - middle_finger_tip[1] > 0 and \
                     ring_finger_pip[1] - ring_finger_tip[1] > 0 and index_finger_pip[1] - index_finger_tip[1] < 0 \
                         and abs(thumb_pip[1] - thumb_tip[1]) > 0 and distancia_euclidiana(index_finger_tip, thumb_tip) <65:
-                    print("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFF")
-                    cv2.putText(image, 'F', (700, 150), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 
-                                3.0, (0, 0, 255), 6)
+                    print("letra detectada ===== F")
+
                     
                 elif abs(index_finger_tip[1] - middle_finger_tip[1]) < 30 and \
                     index_finger_tip[1] < index_finger_pip[1] and \
@@ -166,23 +152,17 @@ with mp_hands.Hands( #modelo de manos
                     ring_finger_tip[1] > ring_finger_pip[1] and \
                     pinky_tip[1] > pinky_pip[1] and \
                     thumb_tip[1] > thumb_pip[1]:
-                    print("hhhhhhhhhhhhhhhhhh")
-                    cv2.putText(image, 'H', (700, 150), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 
-                                3.0, (0, 0, 255), 6)
+                    print("letra detectada ===== H")
+
                 
                 elif abs(pinky_tip[0] - pinky_pip[0]) > 50 and \
                         abs(pinky_tip[1] - pinky_pip[1]) < 20:  # Meñique en posición neutral en el eje vertical
-                        print("jjjjjjjjjjjjjjjjjj")
-                        cv2.putText(image, 'J', (700, 150), 
-                                    cv2.FONT_HERSHEY_SIMPLEX, 
-                                    3.0, (0, 0, 255), 6)
+                        print("letra detectada ===== J")
+
 
                 elif pinky_tip[1] < pinky_pip[1]:  # Meñique extendido hacia arriba
-                    print("iiiiiiiiiiiiiiiiii")
-                    cv2.putText(image, 'I', (700, 150), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 
-                                3.0, (0, 0, 255), 6)
+                    print("letra detectada ===== I")
+
                     
                     
                 elif abs(index_finger_tip[1] - middle_finger_tip[1]) < 30 and \
@@ -192,10 +172,8 @@ with mp_hands.Hands( #modelo de manos
                     pinky_tip[1] > pinky_pip[1] and \
                     abs(thumb_tip[0] - middle_finger_pip[0]) < 30 and \
                     thumb_tip[1] > middle_finger_pip[1]:  # Pulgar debajo de la base del medio
-                    print("kkkkkkkkkkkkkkkkkk")
-                    cv2.putText(image, 'K', (700, 150), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 
-                                3.0, (0, 0, 255), 6)
+                    print("letra detectada ===== K")
+
                     
                 elif abs(index_finger_tip[0] - thumb_tip[0]) < 50 and \
                     index_finger_tip[0] < index_finger_pip[0] and \
@@ -203,31 +181,21 @@ with mp_hands.Hands( #modelo de manos
                     middle_finger_tip[0] > middle_finger_pip[0] and \
                     ring_finger_tip[0] > ring_finger_pip[0] and \
                     pinky_tip[0] > pinky_pip[0]:
-                    print("llllllllllllllllll")
-                    cv2.putText(image, 'L', (700, 150), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 
-                                3.0, (0, 0, 255), 6)
-                    
+                    print("letra detectada ===== L")   
                     
                 elif abs(index_finger_tip[1] - middle_finger_tip[1]) < 30 and \
                     index_finger_tip[1] > index_finger_pip[1] and \
                     middle_finger_tip[1] > middle_finger_pip[1] and \
                     ring_finger_tip[1] < ring_finger_pip[1] and \
                     thumb_tip[1] > thumb_pip[1]:  # Pulgar doblado hacia la palma
-                    print("nnnnnnnnnnnnnnnnnn")
-                    cv2.putText(image, 'N', (700, 150), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 
-                                3.0, (0, 0, 255), 6)
+                    print("letra detectada ===== N")
 
 
                 elif (index_finger_tip[1] > index_finger_pip[1]  # Índice apuntando hacia abajo
                     and middle_finger_tip[1] > middle_finger_pip[1]  # Medio apuntando hacia abajo
                     and ring_finger_tip[1] > ring_finger_pip[1]  # Anular apuntando hacia abajo
                     and distancia_euclidiana(thumb_tip, thumb_pip) < 50):  # Pulgar empuñado
-                    print("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM")
-                    cv2.putText(image, 'M', (700, 150), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 
-                                3.0, (0, 0, 255), 6)
+                    print("letra detectada ===== M")
                                         
                 
                         
@@ -236,10 +204,7 @@ with mp_hands.Hands( #modelo de manos
                     index_finger_tip[1] > index_finger_pip[1] and \
                     middle_finger_tip[1] > middle_finger_pip[1] and \
                     thumb_tip[1] > thumb_pip[1]:
-                    print("pppppppppppppppppp")
-                    cv2.putText(image, 'P', (700, 150), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 
-                                3.0, (0, 0, 255), 6) 
+                    print("letra detectada ===== P")
                     
                     
                 elif index_finger_tip[1] > index_finger_pip[1] and \
@@ -247,13 +212,8 @@ with mp_hands.Hands( #modelo de manos
                     ring_finger_tip[1] > ring_finger_pip[1] and \
                     pinky_tip[1] > pinky_pip[1] and \
                     thumb_tip[1] > thumb_pip[1]:
-                    print("ssssssssssssssssss")
-                    cv2.putText(image, 'S', (700, 150), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 
-                                3.0, (0, 0, 255), 6)
-                                
-                #print("pulgar", thumb_tip[1])
-                #print("dedo indice",index_finger_tip[1])
+                    print("letra detectada ===== S")
+                    
                 
     cv2.imshow('MediaPipe Hands', image)
     if cv2.waitKey(5) & 0xFF == 27:
